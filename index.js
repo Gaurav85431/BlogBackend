16c00f8aef8a5c547cf0cb6d506f35dbdc962919
Gaurav Pushpam <145956263+Gaurav85431@users.noreply.github.com>
2024-07-24 20:11:28 +0530

    firdst commit

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use('/api/user', router)
 app.use('/api/blog', blogRouter)
 
-mongoose.connect('mongodb://127.0.0.1:27017/enthusia-mern-blog');
+// mongoose.connect('mongodb://127.0.0.1:27017/enthusia-mern-blog');
+mongoose.connect('mongodb+srv://pushpamgaurav3:tUgPn725r9bcu9yn@cluster0.1v3tnoq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
 
 app.listen(5000, () => {
   console.log('Server running on port 5000')
